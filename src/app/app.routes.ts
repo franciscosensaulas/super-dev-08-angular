64cca1a5974b4d196a30226c0393a1917372ef4e
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2026-04-25 10:04:45 -0300

    Aula

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "pacientes", loadComponent: () => import('./paciente/paciente').then(x => x.PacienteComponent) },
+    { path: "calculadora", loadComponent: () => import('./calculadora/calculadora').then(x => x.Calculadora)},
+];
